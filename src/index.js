ea11ca14461e606fcc745e4f4cf4ff226394db29
Oluwasegun <arpeiks@gmail.com>
2021-03-05 07:19:25 +0100

    added fonts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
+import "./fonts/SofiaProLightAz.otf";
+import "./fonts/SofiaProMediumAz.otf";
+import "./fonts/SofiaProRegularAz.otf";
+import "./fonts/SofiaProSemiBoldAz.otf";
+import "./fonts/SofiaProExtraLightAz.otf";
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
